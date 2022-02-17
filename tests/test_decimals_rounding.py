422f1081804ff05e8dff503733b7f4ea57d86ad7
9homme <saravuth.r@gmail.com>
2022-02-17 07:42:29 +0700

    test: add some unit test for helper function

--- tests/test_decimals_rounding.py
+++ tests/test_decimals_rounding.py
@@ -14,3 +14,8 @@ def test_rounding_down_with_number_more_than_five_should_round_down():
 def test_rounding_down_with_decimal_more_than_two():
     result = helper.round_decimals_down(2.12345, 4)
     assert result == 2.1234
+
+
+def test_rounding_down_with_decimal_less_than_two():
+    result = helper.round_decimals_down(2.1, 2)
+    assert result == 2.1
